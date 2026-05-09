266633faf5c058006b8b959d99828a1a6264d829
Rajbir <co22353@ccet.ac.in>
2026-05-09 15:39:03 +0530

    Automate admin creation and data seeding for deployment

--- build.sh
+++ build.sh
@@ -6,6 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-
-# Optional: Seed data if you want it to happen on every deploy
-# python manage.py seed_data
+python create_admin.py
+python manage.py seed_data
